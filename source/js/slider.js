bac1d72a7ca738bee056441a21c847f023d1a02e
ilebedev-ru <wapstyle@bk.ru>
2018-07-18 20:42:08 +0500

    домучил слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,220 +1,88 @@
 'use strict';
 
-function Slider( element ) {
-  this.el = document.querySelector( element );
-  this.init();
-}
 
-Slider.prototype = {
-  init: function() {
-    this.links = this.el.querySelectorAll( ".products__point" );
-    this.wrapper = this.el.querySelector( ".products__slider" );
-    this.navigate();
-  },
-  navigate: function() {
+var productItems = document.querySelectorAll('.products__items');
+var productPoint = document.querySelectorAll('.products__point');
 
-    for( var i = 0; i < this.links.length; ++i ) {
-      var link = this.links[i];
-      this.slide( link );
-    }
-  },
+var slideNumber = 0;
 
-  animate: function( slide ) {
-    var parent = slide.parentNode;
-    var caption = slide.querySelector( ".caption" );
-    var captions = parent.querySelectorAll( ".caption" );
-    for( var k = 0; k < captions.length; ++k ) {
-      var cap = captions[k];
-      if( cap !== caption ) {
-        cap.classList.remove( "visible" );
+var changeSlide = function(slides, points) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.add('products__items--active');
+    slides[i].classList.remove('products__items--not-active');
+    points[i].classList.add('products__point--active');
+  }
+
+  slideNumber += 1;
+
+  if (slideNumber < slides.length) {
+    for (var j = 0; j < slides.length; j++) {
+      if (!(j === slideNumber)) {
+        slides[j].classList.remove('products__items--active');
+        slides[j].classList.add('products__items--not-active');
+        points[j].classList.remove('products__point--active');
       }
     }
-    caption.classList.add( "visible" );
-  },
-
-  slide: function( element ) {
-    var self = this;
-    element.addEventListener( "click", function( e ) {
-      e.preventDefault();
-      var a = this;
-      self.setCurrentLink( a );
-      var index = parseInt( a.getAttribute( "data-slide" ), 10 ) + 1;
-      var currentSlide = self.el.querySelector( ".slide:nth-child(" + index + ")" );
-
-      self.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
-      self.animate( currentSlide );
-
-    }, false);
-  },
-  setCurrentLink: function( link ) {
-    var parent = link.parentNode;
-    var a = parent.querySelectorAll( "a" );
-
-    link.className = "current";
-
-    for( var j = 0; j < a.length; ++j ) {
-      var cur = a[j];
-      if( cur !== link ) {
-        cur.className = "";
+  } else {
+    slideNumber = 0;
+    for (var j = 0; j < slides.length; j++) {
+      if (!(j === slideNumber)) {
+        slides[j].classList.remove('products__items--active');
+        slides[j].classList.add('products__items--not-active');
+        points[j].classList.remove('products__point--active');
       }
     }
   }
+  //смена положения пагинации, если номер слайда четный
+  if((slideNumber+1) % 2 == 0) {
+    points[slideNumber].parentNode.className += ' products__pagination--left';
+  } else {
+    //удаляем класс для левого положения, если номер слайда не четный
+    points[slideNumber].parentNode.classList.remove('products__pagination--left');
+  };
+}
+
+//запускаем функцию автоматического переключения слайдов без клика
+var startSlideAutoChange = setInterval(changeSlide, 5000, productItems, productPoint, slideNumber);
+
+
+var changeSlideToClick = function() {
+  var dataId = event.target.getAttribute('data-id'); //получаем data id нажатой кнопки
+  slideNumber = dataId-2; //преобразуем для передачи в функцию по смене слайдов
+
+  changeSlide(productItems, productPoint);
+}
+
+for (var i = 0; i < productPoint.length; i++) {
+  productPoint[i].addEventListener("click", function () {
+    clearInterval(startSlideAutoChange);   //останавливаем автоматическую крутилку
+    changeSlideToClick();
+  } );
 };
 
-document.addEventListener( "DOMContentLoaded", function() {
-  var aSlider = new Slider( "#slider" );
-
-});
-
-
-
-
-
-
-
-
-
-
-
-// var productItems = document.querySelectorAll('.products__items');
-// var productPoint = document.querySelectorAll('.products__point');
-
-
-// for (var i = 0 ; i < productItems.length; i++) {
-//   console.log('Цикл:' + i);
-//   productPoint[i].addEventListener("click" , function(event) {console.log("Это блять " + i)});
-// };
-
-
-// function removeVH() {
-
-//   console.log("Функция сработала");
-//   slide[i].classList.remove('visually-hidden');
-// };
-
-
-
-
-      //в момент клика снова проходим по слайдам и удаляем ВХ
-      // for (var i = 0 ; i < productItems.length; i++) {
-      //   // productItems[i].classList.remove('visually-hidden');
-
-      // }
-
-
-// productPoint[1].addEventListener( "click" , function() {
-//   console.log('i' + productItems[1]);
-//   productItems[1].classList.remove('visually-hidden');
-// });
-
-
-// for (var i = 0 ; i < productItems.length; i++) {
-//   console.log(productItems[i]);
-//   removeVH(productItems, i);
-// }
-
-
-
-
-// тут способ с заранее заданной функцией и ее вызовом (не работает - запускается без клика)
-
-
-// for (var i = 0 ; i < productItems.length; i++) {
-//   productPoint[i].addEventListener( "click" , removeVH(productItems, i));
-// }
-
-
-
-
-
-
-
-
-
-// var productPoint = document.querySelectorAll('products__point');
-
-// for (var i = 0 ; i < productPoint.length; i++) {
-//   productPoint[i].addEventListener( "click" , function() {
-//     var productItems = document.querySelectorAll('products__items');
-//     productItems[i].classList.remove('.visually-hidden');
-//   });
-
-// }
-
-
-
-// var showSlide = function() {
-//   var productItems = document.querySelectorAll('products__items');
-//   var productPoint = document.querySelectorAll('products__point');
-
-//   for (var i = 0 ; i < productItems.length; i++) {
-//   console.log('1:' + productItems[i]);
-//     productPoint[i].addEventListener( "click" , function(){
-//       var slides = [];
-//       slides[i] = productItems[i];
-//       console.log('2:' + productItems[i])
-//       slides[i].classList.remove('.visually-hidden');
-//     });
-//   }
-// }
-
-// showSlide();
-
-
-// var productPagination = document.querySelector('.products__pagination');
-
-// // console.log('1:' + productItems[0]);
-
-// var toggleVHidden = function (slide, i) {
-//   slide[i].classList.remove('visually-hidden');
-// };
-
-
-// var showSlide = function (slides, point) {
-//   for (var i = 0 ; i < slides.length; i++) {
-//     point[i].addEventListener( "click" , function(){
-//       slides[i].classList.remove('visually-hidden');
-//     });
-//   }
-// };
-
-
-
-
-
-
-// showSlide(productItems, productPoint);
-
-
-
-// for (var i = 0 ; i < productItems.length; i++) {
-//   productPoint[i].addEventListener( "click" , function(){slide[i].classList.remove('visually-hidden');}); //toggleVHidden(productItems, i)
-// };
-
-
-// var showSlide = function(slides) {
-//   console.log(slides[i].classList);
-//   slides[i].classList.remove('visually-hidden');
-//   // for (var i = 0 ; i < slides.length; i++) {
-//   //   if(!slides[i].classList.contains('visually-hidden')) {
-//   //     slides[i].classList.add('visually-hidden');
-//   //   }
-//   // }
-// };
-
-
-
-// for (var i = 0 ; i < productItems.length; i++) {
-//    productPoint[i].addEventListener( "click" , showSlide(productItems));
-// };
-
-// for
-
-//допустим сделаем функцию смены слайдов
-// var changeSlide = function () {
-//   productItems[]
-// }
-
-
-// 1 слайд - 1 кнопка.
-//Нажимаем на кнопку - убираем невидимость у текущего слайда, добавляем невидимость всем другим слайдам.
+/////////////////////////////
+/// ПЕРЕКЛЮЧАТЕЛЬ в  features
+/////////////////////////////
+
+var featuresItems = document.querySelectorAll('.features__item');
+var activeItems = 0;
+
+var toggleFeaturesItems = function(items) {
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('features__item--active')) {
+      activeItems = i;
+      items[i].classList.remove('features__item--active');
+    }
+  }
+
+  activeItems +=1;
+
+  if (activeItems < items.length) {
+    items[activeItems].classList.add('features__item--active');
+  } else {
+    activeItems = 0;
+     items[activeItems].classList.add('features__item--active');
+  }
+}
+
+setInterval(toggleFeaturesItems, 4000, featuresItems);
